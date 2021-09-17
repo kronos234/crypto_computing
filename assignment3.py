94a7dffb884c9ef12bf6fbfaacdfd13ee6a768f4
kronos234 <pancholimahak@gmail.com>
2021-09-17 13:53:13 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -4,34 +4,61 @@ import random
 from pprint import pprint
 
 #Truth table for blood type function
-T = [ [1,0,0,0,0,0,0,0],
-          [1,1,0,0,0,0,0,0],
-          [1,0,1,0,0,0,0,0],
-          [1,1,1,1,0,0,0,0],
-          [1,0,0,0,1,0,0,0],
-          [1,1,0,0,1,1,0,0],
-          [1,0,1,0,1,0,1,0],
-          [1,1,1,1,1,1,1,1]
+         #O-,O+,A-,A+,B-,B+,AB-,AB+
+T =     [ [1,0,0,0,0,0,0,0], #O-
+          [1,1,0,0,0,0,0,0], #O+
+          [1,0,1,0,0,0,0,0], #A-
+          [1,1,1,1,0,0,0,0], #A+
+          [1,0,0,0,1,0,0,0], #B-
+          [1,1,0,0,1,1,0,0], #B+
+          [1,0,1,0,1,0,1,0], #AB-
+          [1,1,1,1,1,1,1,1]  #AB+
         ]
+
+def build_input(b_2,b_1,b_0):
+    #add conditions for x_A,...
+    if  ((b_1 = 1)and (b_0 = 1)) or((b_2=1)and(b_1=1)and(b_0=1)):#xA is 1 if the group is A or AB and 0 otherwise
+        b_A = 1
+    else:
+        b_A = 0
+
+    if ((b_2=1)and(b_0=1)) or ((b_2=1)and(b_1=1)and(b_0=1)): #xB is 1 if the group is B or AB and 0 otherwise
+        b_B=1
+    else:
+        b_B =0
+    #bit xR is 1 if the Rh is + and 0 otherwise.
+    if ((b_2=0)and(b_1=0)and(b_0=1)) or ((b_2=0)and(b_1=1)and(b_0=1)) or ((b_2=1)and(b_1=0)and(b_0=1)) or ((b_2=1)and(b_1=1)and(b_0=1)):
+        b_R = 1
+    else:
+        b_R = 0
+
+    return [b_A,b_B,b_R]
 
 class Alice:
     #initialization for Alice
     def __init__(self,n,x,D):
         self.n = n
-        self.ret = D.RandA()
-        self.r = self.ret[0]
-        self.M_A = self.ret[1]
+        #Note that u_B,v_B,w_B here are arrays of length 5
+        self.triple = D.TripleA()
+        self.u_A = self.triple[0]
+        self.v_A = self.triple[1]
+        self.w_A = self.triple[2]
         self.input = x
         self.out = None
 
+    def share_input(self):
+
+    def xor(self):
+
+    def and(self):
+
+    def mult_by_const(self):
+
+    def add_by_const(self):
+
     def send(self):
-        self.u = (self.input+self.r)%2**self.n
-        return self.u
 
     def receive(self, msg):
-        self.v = msg[0]
-        self.z_B = msg[1]
-        self.out = self.M_A[self.u][self.v]^self.z_B
 
     def output(self):
         return self.out
@@ -39,57 +66,91 @@ class Alice:
 class Bob:
     def __init__(self,n,y,D):
         self.n = n
-        self.ret = D.RandB()
-        self.s = self.ret[0]
-        self.M_B = self.ret[1]
+        #Note that u_B,v_B,w_B here are arrays of length 5
+        self.triple = D.TripleB()
+        self.u_B = self.triple[0]
+        self.v_B = self.triple[1]
+        self.w_B = self.triple[2]
         self.input = y
-        self.z_B = None
+        self.out = None
+
+    def share_input(self):
+        self.inputy_B = random.getrandbits(self.n)
+        self.inputy_A = self.inputy_B^self.input
+
+    def xor(self):
+
+    def and(self):
+
+    def mult_by_const(self):
+
+    def add_by_const(self):
 
     def send(self):
         return [self.v,self.z_B]
 
     def receive(self,msg):
-        self.u = msg
-        self.v = (self.input+self.s)%(2**n)
-        self.z_B = self.M_B[self.u][self.v]
+
+    def output(self):
 
 
 
 
-class Dealer:
+class Dealer: # generates random beaver triples
     def __init__(self,n):
-        self.M_A = [ [ 0 for i in range(2**n) ] for j in range(2**n) ]
-        self.M_B = [ [ 0 for i in range(2**n) ] for j in range(2**n) ]
         self.n = n
-        #initialization for Alice
-        self.r = random.getrandbits(self.n)
-        for i in range(2**self.n):
-            for j in range(2**self.n):
-                self.M_A[i][j]=random.getrandbits(1)
-        #initialization for Bob
-        self.s = random.getrandbits(self.n)
-        for i in range(2**self.n):
-            for j in range(2**self.n):
-                self.M_B[i][j] = self.M_A[i][j]^T[i-self.r%(2**self.n)][j-self.s%(2**self.n)]
+        self.u = [0 for i in range(5)]
+        self.v = [0 for i in range(5)]
+        self.w = [0 for i in range(5)]
 
-    def RandA(self):
-        return [self.r,self.M_A]
+    def generate_triple(self):
+        #for loop for number of and gates in the formula:
+        for i in range(5):
+            self.u[i] = random.getrandbits(self.n) #get random u
+            self.v[i] = random.getrandbits(self.n) #get random v
+            self.w[i] = u&v
 
-    def RandB(self):
-        return [self.s,self.M_B]
+    def share_triple(self):
+        for i in range(5):
+            self.u_A[i] = random.getrandbits(self.n)
+            self.u_B[i] = u^self.u_A[i]
+            self.v_A[i] = random.getrandbits(self.n)
+            self.v_B[i] = v^self.v_A[i]
+            self.w_A[i] = random.getrandbits(self.n)
+            self.w_B[i] = w^self.w_A[i]
+
+    def TripleA(self):
+        return [self.u_A,self.v_A,self.w_A]
+
+    def TripleB(self):
+        return [self.u_B,self.v_B,self.w_B]
 
 
 
 if __name__ == '__main__':
     n = 3
+    #formula: (1^(x_A&(1^y_A)))&(1^(x_B&(1^y_B)))&(1^(x_R&(1^y_R)))
     print("")
     print("Blood type is encoded as: O-:000,...,AB+:111")
     print("Alice's output is:")
-    for x in range(8):
-        for y in range(8):
-            D = Dealer(n)
-            A = Alice(n,x,D) #get setup information for Alice from dealer
-            B = Bob(n,y,D) #get setup information for Bob from dealer
+    for x_2 in range(2):
+        for x_1 in range(2):
+            for x_0 in range(2):
+                for y_2 in range(2):
+                    for y_1 in range(2):
+                        for y_0 in range(2):
+                            D = Dealer(n)
+                            input_x = build_input(x_2,x_1,x_0)
+                            input_y = build_input(y_2,y_1,y_0)
+                            A = Alice(n,input_x,D) #beaver triples created for Alice
+                            B = Bob(n,input_y,D)   #beaver triples created for Alice
+                            #Sharing input
+                            B.receive(A.send(A.share_input())) #Alice shares her input
+                            A.receive(B.send(B.share_input())) #Bob shares his input
+
+
+
+
             B.receive(A.send()) #Bob receives first message from Alice
             A.receive(B.send()) #Alice receives second round message from Bob
             print("Compatibility of: ", x, " and ", y, " is ", A.output()) #Alice computes output
